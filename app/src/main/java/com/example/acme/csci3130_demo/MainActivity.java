62038dd99f6eb15fc9e3d3d9f8185cd304972b2c
samdoiron <sam.doiron96@gmail.com>
2018-01-22 22:04:00 -0400

    Add a button to set label text

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText theEditText = (EditText)findViewById(R.id.theEditText);
+        TextView theLabel = (TextView)findViewById(R.id.theLabel);
+        Button theButton = (Button)findViewById(R.id.theButton);
+
+        theButton.setOnClickListener((_e) -> {
+            theLabel.setText(theEditText.getText());
+        });
     }
 }
